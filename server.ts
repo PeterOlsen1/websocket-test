cc20bb2e875fa56dec833c004fea5ba6c5c50217
Peter Olsen <olse0321@umn.edu>
2025-05-21 19:13:35 -0500

    add rooms, redecorate homepage

--- server.ts
+++ server.ts
@@ -1,58 +1,110 @@
 import { WebSocketServer } from 'ws';
 import { v4 as uuid } from 'uuid';
+import { INVALID_ROOM_ERR } from './src/lib/constants.ts'
+
+
+interface Room {
+    clients: Record<string, WebSocket>
+    created: Date
+}
 
 const wss = new WebSocketServer({ port: 8080 });
 
 const users: {[key: string]: string} = {};
-const clients: {[key: string]: WebSocket} = {};
-let broadcaster: WebSocket | null = null;
+const allClients: {[key: string]: WebSocket} = {};
+const rooms: {[key: string]: Room} = {};
+
+/**
+ * Generate a random room id
+ */
+function generateRoomId() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let id = '';
+    for (let i = 0; i < 8; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        id += chars[randomIndex];
+    }
+    return id;
+}
+
+function makeGetClients(roomId: string): () => WebSocket[] | undefined {
+    return () => {
+        if (!roomId) { return; }
+        const c = rooms[roomId]?.clients;
+        if (!c) { return; }
+        return Object.values(c);
+    } 
+}
 
 wss.on('connection', (ws) => {
     // save client to clients map
     const id = uuid();
     console.log('new client: ' + id);
     let username = id;
+    let roomId: string = '';
     users[id] = username;
-    clients[id] = ws;
+    allClients[id] = ws;
+    let getClients: () => WebSocket[] | undefined;
 
 
     ws.on('message', (d) => {
         let data;
         try {
-            data = JSON.parse(d);
+            data = JSON.parse(d as any);
         }
         catch (e) {
             console.log("failed to parse JSON");
             console.log(e);
             ws.send(JSON.stringify({
-                'type': 'error',
-                'error': e
+                type: 'error',
+                error: e
             }));
             return;
         }
 
-        if (data.to) {
-            //see if we need to send to broadcaster or a specific client
-            let toSocket: WebSocket;
-            if (data.to == 'broadcaster') {
-                toSocket = broadcaster;
-            }
-            else {
-                toSocket = clients[data.to];
-            }
-
+        if (data.to) { // we have a specific recipient
+            let toSocket = allClients[data.to];
             if (toSocket) {
                 toSocket.send(JSON.stringify({ ...data, from: id }));
             }
         }
 
-        else if (data.type == 'username') {
+        else if (data.type == 'start') { // room start
+            //initialize room and function to gather all clients
+            const room: Room = {
+                clients: {},
+                created: new Date()
+            }
+            roomId = generateRoomId();
+            while (rooms[roomId]) { //generate unused id
+                roomId = generateRoomId();
+            }
+            rooms[roomId] = room;
+            getClients = makeGetClients(roomId);
+
+            //notify the user that the room has started
+            ws.send(JSON.stringify({
+                type: 'start',
+                roomId: roomId
+            }));
+        }
+        else if (data.type == 'end') {
+            const roomId = data.roomId;
+            delete rooms[roomId];
+        }
+
+        else if (data.type == 'username') { //update username
             users[id] = data.username;
             username = data.username;
         }
-        else if (data.type == 'message') {
+        else if (data.type == 'message') { //send chat
             //send to each so that the message displays on the user too
-            wss.clients.forEach((c) => {
+            if (!roomId) { return; }
+
+            const clients = getClients?.();
+            if (!clients) { return; }
+
+            clients.forEach((c: WebSocket) => {
                 c.send(JSON.stringify({
                     'type': 'message',
                     'message': data.message,
@@ -60,34 +112,67 @@ wss.on('connection', (ws) => {
                 }));
             });
         }
-        else if (data.type == 'join') {
+        else if (data.type == 'join') { // join room
+            roomId = data.roomId;
+            if (!rooms[roomId]) {
+                console.log("Joined invalid room!");
+                ws.send(JSON.stringify({
+                    type: 'error',
+                    error: 'INVALID_ROOM'
+                }));
+                return;
+            }
+
+            //manually fetch clients here since we need keys, not just values
+            const clients = rooms[roomId]?.clients;
+            if (!clients) {
+                return;
+            }
+            clients[id] = ws;
+
+            //create new user object and send it
             const newUser = JSON.stringify({
                 type: 'join',
                 position: wss.clients.size,
-                // broadcaster: wss.clients.size == 1,
                 id,
                 users: Object.keys(clients).filter(key => key != id)
             });
 
-            //user joined the call, send to everyone!
-            wss.clients.forEach(c => {
-                if (c.readyState == ws.OPEN) {
-                    c.send(newUser);
-                }
+            Object.values(clients).forEach(c => {
+                c.send(newUser);
             });
         }
-        else {
-            //send to everyone but the sender
-            wss.clients.forEach((c) => {
+        else { // idk what makes it here
+            if (!roomId) {
+                return;
+            }
+
+            const clients = getClients?.();
+            if (!clients) { return; }
+
+            clients.forEach(c => {
                 if (c.readyState === ws.OPEN) {
                     c.send(JSON.stringify({ ...data, from: id }));
                 }
-            })
+            });
         }
     });
 
+
+    //cleanup on close
     ws.on('close', () => {
         delete users[username];
-        delete clients[id];
+        delete allClients[id];
+
+        const clients = getClients?.();
+        if (!clients) { return; }
+
+        //delete entire room if this is the only one left, otherwise just delete client
+        if (clients.length == 1) {
+            delete rooms[roomId];
+        }
+        else {
+            delete rooms[roomId].clients[id];
+        }
     });
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
     import { onMount } from "svelte";
 
     let responses = $state([""]);
@@ -210,4 +210,150 @@
         style="transform: scaleX(-1);"
     ></video>
     <br>
+</div> -->
+
+<script>
+    import { goto } from "$app/navigation";
+    import { WEBSOCKET_URL } from "$lib/constants";
+    const ws = new WebSocket(WEBSOCKET_URL);
+
+    ws.onmessage = (event) => {
+        let data;
+        try {
+            data = JSON.parse(event.data);
+        }
+        catch (e) {
+            console.error("Could not parse JSON!");
+            return;
+        }
+
+        if (data.type == 'start') {
+            const roomId = data.roomId;
+            ws.close();
+            goto(`/${roomId}`);
+        }
+    }
+
+    function startRoom() {
+        ws.send(JSON.stringify({
+            type: 'start'
+        }));
+    }
+</script>
+
+<style>
+    .main {
+        width: 100%;
+        overflow-x: hidden;
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+    }
+
+    .header {
+        width: 100%;
+        padding-top: 1em;
+        font-size: 4em;
+        text-align: start;
+        /* padding-left: 1em; */
+        font-weight: 600;
+        background-color: white;
+        color: black;
+
+        .header-text {
+            padding-left: 2em;
+        }
+
+        .header-subtext {
+            font-size: 0.25em;
+            padding-left: 16em;
+            color: rgb(48, 48, 48);
+        }
+    }
+
+    .svg-wrapper {
+        z-index: 1;
+    }
+
+    .text-container {
+        width: 90%;
+        text-align: left;
+        margin-left: auto;
+        margin-right: auto;
+        z-index: 3;
+    }
+
+    .body {
+        width: 100%;
+        min-height: 35vh;
+
+        .title {
+            width: 100%;
+            text-align: center;
+            font-size: 1.5em;
+            font-weight: 600;
+            margin-bottom: 1em;
+        }
+    }
+    .white {
+        background-color: white;
+        color: black;
+        min-height: 70vh;
+    }
+</style>
+
+<div class="main">
+    <div class="header">
+        <div class="header-text">
+            Peter's Zoom.
+        </div>
+        <div class="header-subtext">
+            An all new way to video conference
+        </div>
+        <!-- <div class="underline"></div> -->
+        <div class="svg-wrapper" style="transform: scaleY(-1) scaleX(-1); position: relative; top: 0.3em;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="rgb(47, 47, 47)" fill-opacity="1" d="M0,160L34.3,149.3C68.6,139,137,117,206,122.7C274.3,128,343,160,411,181.3C480,203,549,213,617,186.7C685.7,160,754,96,823,74.7C891.4,53,960,75,1029,101.3C1097.1,128,1166,160,1234,176C1302.9,192,1371,192,1406,192L1440,192L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path>
+            </svg>
+        </div>
+    </div>
+    <div class="body">
+        <div class="text-container">
+            <br><br>
+            This was created as a project to test my networking skills. Initially, I had planned to just create a websocket
+            based chatting application, which I was able to spin up in around an hour or so. Upon completion of that,
+            I decided I wanted to take it one step further, and challenge myself with some new networking concepts.
+
+            <br><br>
+            This guided me to trying out WebRTC for the first time, which proved to be a tough challenge. Having never
+            done advanced networking that required signaling to establish a connection, learning how to start
+            the WebRTC communication process was a challenge. I ended up creating a siganling server with websockets
+            to allow clients to communicate with one another.
+
+            <br><br>
+            That's enough of me talking, check it out below!
+        </div>
+    </div>
+    <div class="body white">
+        <div class="svg-wrapper" style="position: relative; top: -1em;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="rgb(47, 47, 47)" fill-opacity="1" d="M0,160L34.3,149.3C68.6,139,137,117,206,117.3C274.3,117,343,139,411,149.3C480,160,549,160,617,149.3C685.7,139,754,117,823,112C891.4,107,960,117,1029,112C1097.1,107,1166,85,1234,85.3C1302.9,85,1371,107,1406,117.3L1440,128L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path>
+            </svg>
+        </div>
+        <div class="text-container" style="position: relative; top: -3.5em;">
+            <div class="title">
+                Start your own room, or join any that are open!
+            </div>
+            <div>
+                Start room
+                <button onclick={startRoom}>
+                    Start
+                </button>
+            </div>
+            <div>
+                Join room
+                <input type="text">
+            </div>
+        </div>
+    </div>
 </div>
